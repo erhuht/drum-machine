3254d2da450219b55cf4c8aac5a1bc8495f5907f
erhuht <erhuht15@gmail.com>
2021-03-25 09:55:57 +0200

    Prettier and clear button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-function Square({audio, playing, state, quarter, onClick}) {
+function Square({ audio, playing, state, quarter, onClick }) {
   let status = "off";
-  
+
   React.useEffect(() => {
     const playSound = () => {
       if (playing && state) {
@@ -12,7 +12,7 @@ function Square({audio, playing, state, quarter, onClick}) {
         } else {
           audio.audio.currentTime = 0;
         }
-          
+
         if (audio.choke) {
           if (!audio.choke.paused) {
             audio.choke.audio.pause();
@@ -20,7 +20,7 @@ function Square({audio, playing, state, quarter, onClick}) {
           }
         }
       }
-    }
+    };
     playSound();
     // eslint-disable-next-line
   }, [playing]);
@@ -32,56 +32,52 @@ function Square({audio, playing, state, quarter, onClick}) {
   }
 
   if (quarter) {
-    status += " quarter"
+    status += " quarter";
   }
 
-  return (
-    <button onClick={onClick} className={status + " square"} />
-  )
+  return <button onClick={onClick} className={status + " square"} />;
 }
 
 class Grid extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      buttonState: Array(64).fill(false)
-    }
-    let hat = {audio: new Audio("/op-hat.wav")}; // There must be a better way to do this
-    this.audio = [hat, {audio: new Audio("/cl-hat.wav"), choke: hat}, {audio: new Audio("/snare.wav")}, {audio: new Audio("/kick.wav")}];
-  }
-
-  handleClick(index) {
-    const squares = this.state.buttonState.slice();
-    squares[index] = !squares[index];
-    this.setState({buttonState: squares});
+    this.state = {};
+    let hat = { audio: new Audio("/op-hat.wav") }; // There must be a better way to do this
+    this.audio = [
+      hat,
+      { audio: new Audio("/cl-hat.wav"), choke: hat },
+      { audio: new Audio("/snare.wav") },
+      { audio: new Audio("/kick.wav") },
+    ];
   }
 
   render() {
     const instruments = [];
-    
+
     for (let i = 0; i < 4; i++) {
-      const row = []
+      const row = [];
       for (let j = 0; j < 16; j++) {
-        let index = i*16+j;
-        row.push(<Square
-          playing={index % 16 === this.props.playingSquares && this.props.playing}
-          key={index}
-          state={this.state.buttonState[index]}
-          onClick={() => this.handleClick(index)}
-          audio={this.audio[i]}
-          quarter={index % 4 === 0}
-          />);
+        let index = i * 16 + j;
+        row.push(
+          <Square
+            playing={
+              index % 16 === this.props.playingSquares && this.props.playing
+            }
+            key={index}
+            state={this.props.buttonState[index]}
+            onClick={() => this.props.onClick(index)}
+            audio={this.audio[i]}
+            quarter={index % 4 === 0}
+          />
+        );
       }
       instruments.push(<div key={i}>{row}</div>);
     }
 
-    return (
-      <div className="Grid">{instruments}</div>
-    )
+    return <div className="Grid">{instruments}</div>;
   }
 }
-
 
 class App extends React.Component {
   constructor(props) {
@@ -89,59 +85,90 @@ class App extends React.Component {
     this.state = {
       playing: false,
       playingSquares: -1,
-      value: "120"
-    }
+      value: "120",
+      buttonState: Array(64).fill(false),
+    };
 
-    this.handleChange = this.handleChange.bind(this)
-    this.startInterval = this.startInterval.bind(this)
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.startInterval = this.startInterval.bind(this);
     this.endInterval = this.endInterval.bind(this);
   }
 
-  tick() {
-    const next = (this.state.playingSquares + 1) % 16;
-    this.setState({playingSquares: next});
+  handleClick(index) {
+    const squares = this.state.buttonState.slice();
+    squares[index] = !squares[index];
+    this.setState({ buttonState: squares });
   }
 
-
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
+  }
+
+  handlePlay(forceStop = false) {
+    if (this.state.playing || forceStop) {
+      clearInterval(this.timerId);
+      this.setState({ playing: false, playingSquares: -1 });
+    } else {
+      this.startInterval();
+      this.setState({ playing: true });
+    }
+  }
+
+  clearSquares() {
+    this.setState({ buttonState: Array(64).fill(false) });
+    this.handlePlay(true);
   }
 
   startInterval() {
     this.tick();
-    this.timerId = setTimeout(this.startInterval, Math.min(500,  15000/this.state.value))
+    this.timerId = setTimeout(
+      this.startInterval,
+      Math.min(500, 15000 / this.state.value)
+    );
   }
 
   endInterval() {
-    clearTimeout(this.timerId)
+    clearTimeout(this.timerId);
   }
 
-  handlePlay() {
-    if (this.state.playing) {
-      clearInterval(this.timerId);
-      this.setState({playing: false, playingSquares: -1});
-    } else {
-      this.startInterval();
-      this.setState({playing: true});
-    }
+  tick() {
+    const next = (this.state.playingSquares + 1) % 16;
+    this.setState({ playingSquares: next });
   }
 
   render() {
     return (
-      <div className="App" onKeyDown={(event) => {if (event.key === " ") { this.handlePlay() }}}>
-        <Grid playingSquares={this.state.playingSquares} playing={this.state.playing}/>
+      <div
+        className="App"
+        onKeyDown={(event) => {
+          if (event.key === " ") {
+            this.handlePlay();
+          }
+        }}
+      >
+        <Grid
+          onClick={this.handleClick}
+          playingSquares={this.state.playingSquares}
+          playing={this.state.playing}
+          buttonState={this.state.buttonState}
+        />
         <div className="InputDiv">
+          <button className="Input" onClick={() => this.clearSquares()}>
+            Clear
+          </button>
           <button
-          className={"ButtonContainer Input"}
-          onClick={() => this.handlePlay()}
-          tabIndex={-1}>
-            <div className={(this.state.playing ? " Stop" : " Play")}/>
+            className={"ButtonContainer Input"}
+            onClick={() => this.handlePlay()}
+            tabIndex={-1}
+          >
+            <div className={this.state.playing ? " Stop" : " Play"} />
           </button>
           <input
-          className="Input"
-          type="number"
-          value={this.state.value}
-          onChange={this.handleChange}
+            className="Input"
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
           />
         </div>
       </div>
